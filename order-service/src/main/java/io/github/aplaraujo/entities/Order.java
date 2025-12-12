f68c0fb953688fc1f6145dfd91c4700265f11e8e
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-12 16:38:05 -0300

    Flyway migration and integration tests

--- order-service/src/main/java/io/github/aplaraujo/entities/Order.java
+++ order-service/src/main/java/io/github/aplaraujo/entities/Order.java
@@ -17,9 +17,14 @@ import java.math.BigDecimal;
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "order_number")
     private String orderNumber;
+    @Column(name = "sku_code")
     private String skuCode;
+    @Column(name = "price")
     private BigDecimal price;
+    @Column(name = "quantity")
     private Integer quantity;
 }
---
+++ order-service/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_orders (
+    id BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
+    order_number VARCHAR(255) DEFAULT NULL,
+    sku_code VARCHAR(255),
+    price DECIMAL(19, 2),
+    quantity INT(11)
+);
